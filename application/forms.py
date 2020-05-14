b62d263c95bc12f0621c6439d5338b9fa261ba9d
JSidat <JSidat@qa.com>
2020-05-14 15:42:42 +0000

    add flask-demo functionality

--- application/forms.py
+++ application/forms.py
@@ -2,20 +2,10 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from application.models import Users
+from flask_login import current_user
+
 
 class PostForm(FlaskForm):
-    first_name = StringField('First Name',
-            validators = [
-                DataRequired(),
-                Length(min=2, max=30)
-            ]
-     )
-    last_name = StringField('Last_Name',
-            validators = [
-                DataRequired(),
-                Length(min=2, max=30)
-            ]
-    )
     title = StringField('Title',
             validators = [
                 DataRequired(),
@@ -33,6 +23,18 @@ class PostForm(FlaskForm):
 
 
 class RegistrationForm(FlaskForm):
+    first_name = StringField('First Name',
+            validators = [
+                DataRequired(),
+                Length(min=2, max=30)
+            ]
+        )
+    last_name = StringField('Last Name',
+            validators = [
+                DataRequired(),
+                Length(min=2, max=30)
+            ]
+        )
     email = StringField('Email',
             validators = [
                 DataRequired(),
@@ -71,7 +73,28 @@ class LoginForm(FlaskForm):
                 DataRequired()
             ]
         )
-
+    
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
+
+class UpdateAccountForm(FlaskForm):
+    first_name = StringField('First Name',
+            validators = [
+                DataRequired(),
+                Length(min=2, max=30)
+            ]
+        )
+    last_name = StringField('Last Name',
+            validators = [
+                DataRequired(),
+                Length(min=2, max=30)
+            ]
+        )
+    email = StringField('Email',
+            validators = [
+                DataRequired(),
+                Email()
+            ]
+        )
+    submit = SubmitField('Email already in use')
--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, url_for, request
 from application import app, db, bcrypt
 from application.models import Posts, Users
-from application.forms import PostForm, RegistrationForm, LoginForm
+from application.forms import PostForm, RegistrationForm, LoginForm, UpdateAccountForm
 from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route('/')
@@ -16,11 +16,18 @@ def about():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hash_pw = bcrypt.generate_password_hash(form.password.data)
 
-        user = Users(email=form.email.data, password=hash_pw)
+        user = Users(
+                first_name=form.first_name.data,
+                last_name=form.last_name.data,
+                email=form.email.data, 
+                password=hash_pw
+                )
 
         db.session.add(user)
         db.session.commit()
@@ -36,7 +43,7 @@ def login():
     if form.validate_on_submit():
         user=Users.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember_form.remember.data)
+            login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
             if next_page:
                 return redirect(next_page)
@@ -50,10 +57,9 @@ def post():
     form = PostForm()
     if form.validate_on_submit():
         postData = Posts(
-                first_name=form.first_name.data,
-                last_name=form.last_name.data,
                 title=form.title.data,
-                content=form.content.data
+                content=form.content.data,
+                author=current_user
         )
 
         db.session.add(postData)
@@ -65,10 +71,36 @@ def post():
     return render_template('post.html', title='Post', form=form)
     
 
-
-
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('login'))
 
+@app.route('/account', methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.first_name = form.first_name.data
+        current_user.last_name = form.last_name.user
+        current_user.email = form.email.data
+        db.session.commit()
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.first_name.data = current_user.first_name
+        form.last_name.data = current_user.last_name
+        form.email.data = current_user.email
+    return render_template('account.html', title='Account', form=form)
+
+@app.route('/account/delete', methods=['GET','POST'])
+@login_required
+def account_delete():
+    user = current_user.id
+    account = Users.query.filter_by(id=user).first()
+    posts = Posts.query.filter_by(user_id=user).all()
+    logout_user()
+    for post in posts:
+        db.session.delete(post)
+    db.session.delete(account)
+    db.session.commit()
+    return redirect(url_for('register'))
